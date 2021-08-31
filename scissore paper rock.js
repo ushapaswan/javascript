6c78dcd0c83b86c245581e804c35de835a078421
ushapaswan <82705683+ushapaswan@users.noreply.github.com>
2021-08-31 20:26:04 +0530

    Add files via upload

---
+++ scissore paper rock.js
@@ -0,0 +1,34 @@
+let readlineSync = require("readline-sync");
+ 
+var computerMoves = ['rock', 'paper', 'scissors'];
+var randomMoveOfComputer = computerMoves[Math.floor(Math.random() * computerMoves.length)];
+ 
+var playerName= readlineSync.question("What is your name: ")
+console.log("hi "+ playerName )
+console.log("choose any move in rock paper scissors")
+var playerMove = readlineSync.question("Which move you want to do? ");
+ 
+if (playerMove == randomMoveOfComputer){
+   console.log("Match Draw");
+}
+else if(playerMove =="rock" && randomMoveOfComputer == "paper"){
+   console.log("computer won the match");
+}
+else if(playerMove=="rock" && randomMoveOfComputer == "scissors"){
+    console.log( playerName,"win");
+}
+else if(playerMove=="paper" && randomMoveOfComputer == "scissors"){
+    console.log("computer won the match");
+}
+else if(playerMove=="paper" && randomMoveOfComputer == "rock"){
+    console.log(playerName, "won the match");
+}
+else if(playerMove=="scissors" && randomMoveOfComputer == "paper"){
+    console.log(playerName," won the match");
+}
+else if(playerMove=="scissors" && randomMoveOfComputer == "rock"){
+    console.log("computer won the match");
+}
+// var computerMoves = ['rock', 'paper', 'scissors'];
+// var randomMoveOfComputer = computerMoves[Math.floor(Math.random() * computerMoves.length)];
+// console.log(randomMoveOfComputer)
